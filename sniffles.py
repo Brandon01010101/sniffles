bdaa7c9d8a8178c2cb8976a73eeb7e6286ccad81
Brandon <brandon282859@protonmail.com>
2024-12-23 16:44:29 +0100

    Update sniffles.py

--- sniffles.py
+++ sniffles.py
@@ -76,7 +76,7 @@ def main():
             sniff(prn=analyser_paquet, store=False, timeout=INTERVALLE_ANALYSE)
             verifier_ddos()
         except KeyboardInterrupt:
-            print("\nStopping the program. Thank you for using this tool created by Gentleman 💻👍")
+            print("\nStopping the program. Thank you for using this tool created by https://linktr.ee/Brandon008 💻👍")
             break
 
 if __name__ == "__main__":
